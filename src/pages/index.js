3e42ac9d7189597dcbbfd6f4c76360e72fef07d2
Sachin Hatikankan <shatikankan@gmail.com>
2021-12-05 20:58:08 +0530

    updated home page description

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import { StaticImage } from 'gatsby-plugin-image'
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
-      <p>This site has been created to jot down all the cool stuff which can be done using UiPath</p>
+      <p>This site has been created to jot down all the cool stuff which can be done using UiPath or tech I am interested in or currently learning!</p>
       <StaticImage
         alt="Developer, a person who codes"
         src="C:\Users\Sachin\Gatsby\SachinHatikankanGatsbyPortfolio\src\images\developerImage.jpg"
